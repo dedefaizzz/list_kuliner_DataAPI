33d7f0d1bc31839ec95c44c8bd1409784eb513c2
DEDE CAHYADI <111202113415@mhs.dinus.ac.id>
2023-11-26 21:30:11 +0700

    method factory pada makanan.dart

--- lib/makanan.dart
+++ lib/makanan.dart
@@ -23,6 +23,29 @@ class Makanan {
       required this.bahan,
       required this.deskripsi,
       required this.gambar});
+
+/* Terdapat sebuah method factory dgn nama Makanan.fromJson
+   Method ini digunakan utk membuat objek Makanan dari data yg diterima dlm bentuk Map<String, dynamic>
+   Biasanya digunakan ketika ingin mengonversi data JSON mjd objek dart */
+
+/* Atribut nama, deskripsi, detail, waktubuka, harga, kalori, dan gambar diambil dari nilai-nilai yang sesuai dalam json */
+/* Atribut gambarlain diinisialisasi dengan list yang berisi URL gambar tambahan dari nilai yang ada dalam json */
+/* Atribut bahan diinisialisasi dengan list dari map yang mewakili bahan-bahan. Data ini diambil dari json dan diubah menjadi tipe yang sesuai */
+
+  factory Makanan.fromJson(Map<String, dynamic> json) {
+    return Makanan(
+        nama: json["nama"],
+        deskripsi: json["deskripsi"],
+        detail: json["detail"],
+        waktubuka: json["waktubuka"],
+        harga: json["harga"],
+        kalori: json["kalori"],
+        gambar: json["gambar"],
+        gambarlain: List<String>.from(json["gambarlain"]),
+        bahan: List<Map<String, String>>.from(json["bahan"].map((x) =>
+            Map.from(x)
+                .map((key, value) => MapEntry<String, String>(key, value)))));
+  }
 }
 
 List<Makanan> listMakanan = [
