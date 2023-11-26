ab36066fe01d06d665ee2e8e4f69ec7ee8eb4d23
DEDE CAHYADI <111202113415@mhs.dinus.ac.id>
2023-11-26 21:45:43 +0700

    class permintaan HTTP

---
+++ lib/http_helper.dart
@@ -0,0 +1,11 @@
+/* Class ini digunakan utk melakukan permintaan HTTP ke URL dari web bengkel koding */
+import 'package:http/http.dart' as http;
+
+class HttpHelper {
+  String url = 'https://bengkelkoding.dinus.ac.id/food/';
+  Future<http.Response> getAPI() async {
+    final response =
+        await http.get(Uri.parse('https://bengkelkoding.dinus.ac.id/food/'));
+    return response;
+  }
+}
